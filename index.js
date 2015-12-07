b37420842d4b7e2433096895dfbd7667cc29afd6
John Arthorne <John_Arthorne@ca.ibm.com>
2015-12-07 09:46:13 -0500

    Change message to test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello Node.js on Bluemix!');
+  response.send('Hello from branch test on Bluemix');
 });
 
 app.listen(app.get('port'), function() {
